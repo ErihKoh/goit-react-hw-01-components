9d5e858fc176eaf1c34e4c09406e0325f6465489
Yurii Zubar <barsuk0831@gmail.com>
2020-11-28 14:27:38 +0200

    change names

---
+++ src/components/User_profile/user_prifile.jsx
@@ -0,0 +1,24 @@
+import PropTypes from 'prop-types';
+
+function Profile({ avatar, name, tag, location, children }) {
+  return (
+    <div className="profile">
+      <div className="description">
+        <img src={avatar} alt={name} className="avatar" />
+        <p className="name">{name}</p>
+        <p className="tag">{tag}</p>
+        <p className="location">{location}</p>
+        {children}
+      </div>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  avatar: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+};
+
+export default Profile;

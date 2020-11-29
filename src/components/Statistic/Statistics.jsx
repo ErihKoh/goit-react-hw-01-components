fbfe90073a4418d1290d907f5314edc7380a2962
Yurii Zubar <barsuk0831@gmail.com>
2020-11-29 10:42:15 +0200

    добавил рендер по условию в статистик

--- src/components/Statistic/Statistics.jsx
+++ src/components/Statistic/Statistics.jsx
@@ -4,7 +4,7 @@ import s from './Statistic.module.css';
 function Statistics({ title, children }) {
   return (
     <section className={s.statistics}>
-      <h2 className={s.title}>{title}</h2>
+      {title && <h2 className={s.title}>{title}</h2>}
 
       {children}
     </section>
